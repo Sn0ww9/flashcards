9754f0146c3616d289ae1c1b0a7c3c358b69e3d7
Sn0ww9 <130753801+Sn0ww9@users.noreply.github.com>
2024-11-27 21:38:59 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,27 @@
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'biologia' ,
+    'o que é lisossomos',
+    'organelas celulares que atuam na digestão intracelular, degradando partículas e reciclando componentes celulares'
+)
+
+criaCartao(
+    'biologia',
+    'o que é mitocôndria',
+    'respiração celular'
+)
+criaCartao(
+    'química',
+    'o que é atomo',
+    'sistema energético estável, eletricamente neutro'
+)
+criaCartao(
+    'filosofia',
+    'o que é etica',
+    'compreender a fundamentação das norrmas'
+)
